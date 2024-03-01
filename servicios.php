6cd906c806e28d9454aa93f29a971b72f9c768b3
Josué Martínez Carrasco <josue@jmcsoluciones.com>
2024-02-29 19:15:33 -0600

    Listo servicios tablet

--- servicios.php
+++ servicios.php
@@ -24,37 +24,39 @@ include './components/header.php';
             En ESPECAGUA, ofrecemos una amplia gama de servicios de instalación y mantenimiento de sistemas de agua. Nuestro equipo de expertos se especializa en la instalación y mantenimiento de sistemas de agua para la industria, comercio y hogar. <strong>Con más de 20 años de experiencia, garantizamos eficiencia y durabilidad sin complicaciones.</strong>
             </p>
         </div>
-        <article class="servicios__content__item">
-            <div class="servicios__content__item__text">
-                <h3>Mantenimiento de Sistemas de Riego</h3>
-                <p>El mantenimiento regular de sistemas de riego es esencial para garantizar un rendimiento óptimo. Ignorar este aspecto puede resultar en obstrucciones, pérdida de presión y daño a los cultivos. Nuestro servicio de mantenimiento aborda estos problemas, asegurando que tu sistema funcione eficientemente. <strong>Detectamos y corregimos posibles fallas antes de que se conviertan en costosas reparaciones</strong> , manteniendo tu inversión protegida y tu cosecha floreciendo.</p>
-            </div>
-        </article>
-        <article class="servicios__content__item">
-            <div class="servicios__content__item__text">
-                <h3>Instalación de Filtros</h3>
-                <p>Los filtros son cruciales para mantener la calidad del agua. La falta de filtros adecuados puede llevar a la acumulación de sedimentos y contaminantes, afectando la eficiencia de tu sistema. Nuestra instalación de filtros personalizada aborda estas preocupaciones, utilizando tecnología avanzada para purificar el agua. <strong>Desde filtros de carbón activado hasta filtros multicama</strong>, ofrecemos soluciones adaptadas que mejoran la calidad del agua y prolongan la vida útil de tus equipos.</strong>
-                </p>
-            </div>
-        </article>
-        <article class="servicios__content__item">
-            <div class="servicios__content__item__text">
-                <h3>Suavizadores de Agua</h3>
-                <p>Los suavizadores de agua son fundamentales para prevenir la acumulación de minerales que pueden obstruir tuberías y equipos. Si se ignora esta necesidad, la dureza del agua puede causar corrosión y reducir la eficiencia del sistema. <strong>Nuestro servicio de suavización de agua utiliza tecnología de vanguardia para eliminar minerales no deseados, prolongando la vida de tus equipos y mejorando la eficiencia del riego,</strong> lo que se traduce en un menor consumo de agua y costos operativos reducidos.</p>
-            </div>
-        </article>
-        <article class="servicios__content__item">
-            <div class="servicios__content__item__text">
-                <h3>Instalación de Tuberías</h3>
-                <p>La instalación adecuada de tuberías es crucial para evitar fugas y asegurar la durabilidad del sistema. Tubos mal instalados pueden resultar en pérdida de agua, daño a la infraestructura y costosas reparaciones. <strong>Nuestro equipo altamente capacitado garantiza una instalación precisa, utilizando materiales duraderos y técnicas eficientes.</strong>  Esto no solo previene problemas a corto plazo, sino que también garantiza la integridad a largo plazo de tu sistema, proporcionando un riego confiable y eficiente.</p>
-            </div>
-        </article>
-        <article class="servicios__content__item">
-            <div class="servicios__content__item__text">
-                <h3>Venta de Accesorios y Refacciones</h3>
-                <p>Ofrecemos una amplia gama de accesorios y refacciones, desde boquillas hasta conectores, esenciales para el funcionamiento óptimo de tu sistema. La falta de piezas de repuesto puede resultar en tiempo de inactividad no planificado y costos adicionales. <strong>Nuestra selección de productos de calidad garantiza la disponibilidad de componentes esenciales cuando más los necesitas.</strong>  Con nuestra venta de accesorios y refacciones, puedes estar seguro de que tu sistema estará respaldado con las piezas correctas para un rendimiento continuo y eficiente.</p>
-            </div>
-        </article>
+        <section class="servicios__content__items">
+            <article class="servicios__content__items__item">
+                <div class="servicios__content__items__item__text">
+                    <h3>Mantenimiento de Sistemas de Riego</h3>
+                    <p>El mantenimiento regular de sistemas de riego es esencial para garantizar un rendimiento óptimo. Ignorar este aspecto puede resultar en obstrucciones, pérdida de presión y daño a los cultivos. Nuestro servicio de mantenimiento aborda estos problemas, asegurando que tu sistema funcione eficientemente. <strong>Detectamos y corregimos posibles fallas antes de que se conviertan en costosas reparaciones</strong> , manteniendo tu inversión protegida y tu cosecha floreciendo.</p>
+                </div>
+            </article>
+            <article class="servicios__content__items__item">
+                <div class="servicios__content__items__item__text">
+                    <h3>Instalación de Filtros</h3>
+                    <p>Los filtros son cruciales para mantener la calidad del agua. La falta de filtros adecuados puede llevar a la acumulación de sedimentos y contaminantes, afectando la eficiencia de tu sistema. Nuestra instalación de filtros personalizada aborda estas preocupaciones, utilizando tecnología avanzada para purificar el agua. <strong>Desde filtros de carbón activado hasta filtros multicama</strong>, ofrecemos soluciones adaptadas que mejoran la calidad del agua y prolongan la vida útil de tus equipos.</strong> </p>
+                </div>
+            </article>
+            <article class="servicios__content__items__item">
+                <div class="servicios__content__items__item__text">
+                    <h3>Suavizadores de Agua</h3>
+                    <p>Los suavizadores de agua son fundamentales para prevenir la acumulación de minerales que pueden obstruir tuberías y equipos. Si se ignora esta necesidad, la dureza del agua puede causar corrosión y reducir la eficiencia del sistema. <strong>Nuestro servicio de suavización de agua utiliza tecnología de vanguardia para eliminar minerales no deseados, prolongando la vida de tus equipos y mejorando la eficiencia del riego,</strong> lo que se traduce en un menor consumo de agua y costos operativos reducidos.</p>
+                </div>
+            </article>
+            <article class="servicios__content__items__item">
+                <div class="servicios__content__items__item__text">
+                    <h3>Instalación de Tuberías</h3>
+                    <p>La instalación adecuada de tuberías es crucial para evitar fugas y asegurar la durabilidad del sistema. Tubos mal instalados pueden resultar en pérdida de agua, daño a la infraestructura y costosas reparaciones. <strong>Nuestro equipo altamente capacitado garantiza una instalación precisa, utilizando materiales duraderos y técnicas eficientes.</strong>  Esto no solo previene problemas a corto plazo, sino que también garantiza la integridad a largo plazo de tu sistema, proporcionando un riego confiable y eficiente.</p>
+                </div>
+            </article>
+            <article class="servicios__content__items__item">
+                <div class="servicios__content__items__item__text">
+                    <h3>Venta de Accesorios y Refacciones</h3>
+                    <p>Ofrecemos una amplia gama de accesorios y refacciones, desde boquillas hasta conectores, esenciales para el funcionamiento óptimo de tu sistema. La falta de piezas de repuesto puede resultar en tiempo de inactividad no planificado y costos adicionales. <strong>Nuestra selección de productos de calidad garantiza la disponibilidad de componentes esenciales cuando más los necesitas.</strong>  Con nuestra venta de accesorios y refacciones, puedes estar seguro de que tu sistema estará respaldado con las piezas correctas para un rendimiento continuo y eficiente.</p>
+                </div>
+            </article>
+        </section>
+      
     </section>
     <?php include 'equipos.php';?>
    
